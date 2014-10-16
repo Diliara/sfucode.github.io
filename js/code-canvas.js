a607c599c6ac4d4c346c4aab4f64bde2b34ab092
Diliara <contact@diliaranasirova.com>
2014-10-16 12:23:12 -0700

    Delete all Foundation files

--- js/code-canvas.js
+++ js/code-canvas.js
@@ -1,23 +1,7 @@
 // CODE javascript include
 
 (function () {
-    var script_modernizr = document.createElement('script');
     var script_jquery = document.createElement('script');
-    var script_foundation = document.createElement('script');
-    var script_foundation_init = document.createElement('script');
-
-    //script_jquery.type = 'text/javascript';
-    //script_foundation.type = 'text/javascript';
-
-    script_modernizr.src = 'js/vendor/modernizr.js';
     script_jquery.src = document.location.protocol + '//code.jquery.com/jquery-1.11.1.min.js';
-    script_foundation.src = 'js/foundation.min.js';
-
-    document.getElementsByTagName('head')[0].appendChild(script_modernizr)
     document.body.appendChild(script_jquery);
-    document.body.appendChild(script_foundation);
-
-    script_foundation_init.appendChild(document.createTextNode("$(document).foundation();"));
-    document.body.appendChild(script_foundation_init);
-
 })();
