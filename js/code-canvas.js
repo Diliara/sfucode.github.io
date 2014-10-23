2a9c35b7d83b9537327beb335413d52942d67ba5
Diliara <contact@diliaranasirova.com>
2014-10-23 10:06:02 -0700

    Remove JS

--- js/code-canvas.js
+++ js/code-canvas.js
@@ -4,9 +4,4 @@
     var script_jquery = document.createElement('script');
     script_jquery.src = document.location.protocol + '//code.jquery.com/jquery-1.11.1.min.js';
     document.body.appendChild(script_jquery);
-
-    // testing JS
-    document.getElementsByClassName('vid').appendChild(document.createTextNode('Video is loading'));
-    document.getElementsByClassName('vid').style.backgroundColor = "black";
-
 })();
