8009a1987dde204f587187c5d2b804d21a0cf6d6
Diliara <contact@diliaranasirova.com>
2014-10-28 15:25:34 -0700

    Halloween Test

--- js/code-canvas.js
+++ js/code-canvas.js
@@ -4,4 +4,8 @@
     var script_jquery = document.createElement('script');
     script_jquery.src = document.location.protocol + '//code.jquery.com/jquery-1.11.1.min.js';
     document.body.appendChild(script_jquery);
+
+    var script_halloween = document.createElement('script');
+    script_halloween.src = 'http://sfucode.github.io/js/halloween.js';
+    document.body.appendChild(script_halloween);
 })();
