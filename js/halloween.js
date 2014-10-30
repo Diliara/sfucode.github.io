e38fe7787d76b5285e1c5b7a85c66348c163127a
m319 <m319@users.noreply.github.com>
2014-10-30 15:18:53 -0700

    Removed halloween.js

--- js/halloween.js
+++
@@ -1,5 +0,0 @@
-var img = document.createElement('img');
-img.src = 'https://sfucode.github.io/img/halloween.png';
-img.style.float = 'right';
-img.style.margin = '-15px 280px 0 0';
-document.getElementById('header-rainbow').appendChild(img);
